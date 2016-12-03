0e430f74061ea1db0429322b21021429e2f37309
Jacob <jacobagraff@gmail.com>
2016-12-02 20:15:30 -0500

    pep8 market_methods.py

--- code/app/market_methods.py
+++ code/app/market_methods.py
@@ -8,47 +8,57 @@ import time
 QUERY = "http://localhost:8080/query?id={}"
 ORDER = "http://localhost:8080/order?id={}&side=sell&qty={}&price={}"
 
+
 def get_market_quote():
     try:
-        quote = json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
+        quote = \
+            json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
     except ValueError:
         print 'Failed to get quote from exchange'
         return -1
     return quote
 
+
 def get_market_time():
     try:
-        quote = json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
+        quote = \
+            json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
     except ValueError:
         print 'Failed to get quote from exchange'
         return -1
     timestamp = quote['timestamp']
-    t = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f");
+    t = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
     seconds = time.mktime(t.timetuple())
     return seconds
 
+
 def get_end_of_day_time():
     try:
-        quote = json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
+        quote = \
+            json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
     except ValueError:
         print 'Failed to get quote from exchange'
         return -1
 
     timestamp = quote['timestamp']
     timestamp_modified = timestamp.split(" ")[0] + " 00:00:00.0"
-    t = datetime.datetime.strptime(timestamp_modified, "%Y-%m-%d %H:%M:%S.%f");
+    t = datetime.datetime.strptime(timestamp_modified, "%Y-%m-%d %H:%M:%S.%f")
     seconds = time.mktime(t.timetuple())
     seconds = seconds + ((8 * 60) * 60)
     return seconds
 
+
 def get_market_time_formatted(format_str):
         time_seconds = get_market_time()
-        time_str = datetime.datetime.fromtimestamp(time_seconds).strftime(format_str)
+        time_str = \
+            datetime.datetime.fromtimestamp(time_seconds).strftime(format_str)
         return time_str
+
 
 def get_market_price():
     try:
-        quote = json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
+        quote = \
+            json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
     except ValueError:
         print 'Failed to get quote from exchange'
         return -1
