3cbe17343bd3f482758fc64476a42c0447c9628f
Jacob <jacobagraff@gmail.com>
2016-12-02 20:19:30 -0500

    pep8 multi_processing_handler.py

--- code/app/multi_processing_handler.py
+++ code/app/multi_processing_handler.py
@@ -2,9 +2,11 @@ import database_methods
 from transaction import *
 import multiprocessing
 
+
 def add_workload_to_queue(q, workload):
         database_methods.updateTransactionQueuedStatus(workload[0], True)
         q.put(workload)
+
 
 def process_workload(q):
     while True:
@@ -14,13 +16,12 @@ def process_workload(q):
         execute_transaction(q, workload[0], workload[1], workload[2])
         q.task_done()
 
+
 def execute_transaction(q, new_id, username, order_obj):
     transaction_executer = TransactionExecuter(username, new_id)
     transaction_executer.execute_transaction(order_obj)
     curr_tran = database_methods.getTransactionById(new_id)
-    if (curr_tran.finished == False):
+    if (curr_tran.finished is False):
         remaining_qty = curr_tran.qty_requested - curr_tran.qty_executed
         workload = [new_id, username, order_obj]
-        add_workload_to_queue(q, workload)        
-
-
+        add_workload_to_queue(q, workload)
