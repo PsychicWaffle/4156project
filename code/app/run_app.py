193f9c1780d4f0d1a0f7bbc0ed83af0a353a9e24
Andrew Grant <andrewgrant243@gmail.com>
2016-12-13 20:21:55 -0500

    Sleep after we catch broken socket

--- code/app/run_app.py
+++ code/app/run_app.py
@@ -1,11 +1,14 @@
 import subprocess 
 import socket
+import time
 
 def run_app():
     while (True):
-        process = None
         try:
+            print "About to run python server.py"
             subprocess.call(["python", "server.py"])
+            print "app crashed - about to restart after sleeping"
+            time.sleep(5)
         except socket.error, e:
             print str(e)
             print "App received error - trying again"
